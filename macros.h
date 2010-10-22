7316511ca1199e3252dc701a1981e067ce3cdec9
Frederico Gonçalves <frederico.lopes.goncalves@gmail.com>
2010-10-22 16:24:48 +0100

    errors

--- macros.h
+++ macros.h
@@ -27,7 +27,7 @@
 #define F "FATAL ERROR"
 
 /*
- * Simple macro to malloc data and check if the allocation was successfully.
+ * Simple macro to malloc data and check if the allocation was successfull.
  * A simple use can be char* string = alloc(char, 5); which allocates a string with 5 characters.
  */
 #define alloc(type, how_many)				\
